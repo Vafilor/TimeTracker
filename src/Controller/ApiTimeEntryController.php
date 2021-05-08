2338513b0ebd74677244b71c4736a1360c963524
Andrey Melnikov <vafilor@gmail.com>
2021-05-07 17:38:12 -0700

    feat: updated time entry json API with new get/edit methods and cleaned it up in general. Also updated README with information on datetime format for json API

--- src/Controller/ApiTimeEntryController.php
+++ src/Controller/ApiTimeEntryController.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Api\ApiFormError;
 use App\Api\ApiPagination;
+use App\Api\ApiProblem;
+use App\Api\ApiProblemException;
 use App\Api\ApiTimeEntry;
-use App\Entity\Task;
+use App\Entity\TimeEntry;
 use App\Form\Model\TimeEntryListFilterModel;
+use App\Form\Model\TimeEntryModel;
+use App\Form\TimeEntryFormType;
 use App\Form\TimeEntryListFilterFormType;
-use App\Repository\TaskRepository;
 use App\Repository\TimeEntryRepository;
+use InvalidArgumentException;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,7 +24,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiTimeEntryController extends BaseController
 {
-    #[Route('/api/time-entry', name: 'api_time_entry_index')]
+    #[Route('/api/time-entry', name: 'api_time_entry_index', methods: ["GET"])]
     public function index(
         Request $request,
         TimeEntryRepository $timeEntryRepository,
@@ -65,4 +70,125 @@ class ApiTimeEntryController extends BaseController
 
         return $this->json(ApiPagination::fromPagination($pagination, $items));
     }
+
+    #[Route('/api/time-entry', name: 'api_time_entry_create', methods: ["POST"])]
+    public function create(
+        Request $request,
+        TimeEntryRepository $timeEntryRepository): Response
+    {
+        // TODO allow tags in body. Want to be able to do
+        // --tags=cake,bake,shake
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        $runningTimeEntry = $timeEntryRepository->findRunningTimeEntry($this->getUser());
+        if (!is_null($runningTimeEntry)) {
+            throw new ApiProblemException(
+                ApiProblem::invalidAction(
+                    TimeEntryController::codeRunningTimer,
+                    'You have a running timer',
+                    ['resource' => $runningTimeEntry->getIdString()]
+                )
+            );
+        }
+
+        $timeEntry = new TimeEntry($this->getUser());
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($timeEntry);
+        $manager->flush();
+
+        $data = json_decode($request->getContent(), true);
+        if (!array_key_exists('time_format', $data)) {
+            $timeFormat = 'date';
+        } else {
+            $timeFormat = $data['time_format'];
+        }
+
+        $apiTimeEntry = ApiTimeEntry::fromEntity($timeEntry, $this->getUser(), $timeFormat);
+        $data = [
+            'timeEntry' => $apiTimeEntry,
+            'url' => $this->generateUrl('api_time_entry_view', ['id' => $timeEntry->getIdString()])
+        ];
+
+        return $this->json($data, Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/time-entry/{id}', name: 'api_time_entry_view', methods: ["GET"])]
+    public function view(
+        Request $request,
+        TimeEntryRepository $timeEntryRepository,
+        string $id): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        /** @var TimeEntry|null $timeEntry */
+        $timeEntry = $timeEntryRepository->find($id);
+        if (is_null($timeEntry)) {
+            throw $this->createNotFoundException();
+        }
+
+        $apiTimeEntry = ApiTimeEntry::fromEntity($timeEntry, $this->getUser());
+
+        return $this->json($apiTimeEntry);
+    }
+
+    #[Route('/api/time-entry/{id}', name: 'api_time_entry_edit', methods: ["PUT"])]
+    public function edit(
+        Request $request,
+        TimeEntryRepository $timeEntryRepository,
+        string $id): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        /** @var TimeEntry|null $timeEntry */
+        $timeEntry = $timeEntryRepository->find($id);
+        if (is_null($timeEntry)) {
+            throw $this->createNotFoundException();
+        }
+
+        $form = $this->createForm(TimeEntryFormType::class, TimeEntryModel::fromEntity($timeEntry), [
+            'timezone' => $this->getUser()->getTimezone(),
+            'csrf_protection' => false,
+        ]);
+
+        try {
+            $data = json_decode($request->getContent(), true);
+            $form->submit($data);
+        } catch (InvalidArgumentException $invalidArgumentException) {
+            throw new ApiProblemException(new ApiProblem(Response::HTTP_BAD_REQUEST, ApiProblem::TYPE_VALIDATION_ERROR));
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var TimeEntryModel $data */
+            $data = $form->getData();
+            if ($data->hasDescription()) {
+                $timeEntry->setDescription($data->getDescription());
+            }
+            if ($data->hasStartedAt()) {
+                $timeEntry->setStartedAt($data->getStartedAt());
+            }
+            if ($data->isEnded()) {
+                $timeEntry->setEndedAt($data->getEndedAt());
+            }
+
+            $this->getDoctrine()->getManager()->flush();
+        } elseif (!$form->isValid()) {
+            $formError = new ApiFormError($form->getErrors(true));
+            throw new ApiProblemException($formError);
+        }
+
+        $apiTimeEntry = ApiTimeEntry::fromEntity($timeEntry, $this->getUser());
+
+        return $this->json($apiTimeEntry);
+    }
+
+    // Continue
+    // STOP
+    // RESume
+    // delete
+    // add tag
+    // delete tag
+    // get tags
+    // add task
+    // delete task
+    // today
 }
