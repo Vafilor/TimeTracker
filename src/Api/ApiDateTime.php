8971af1a8db7cd84d9e4b89141a7fdb879ce8132
Andrey Melnikov <vafilor@gmail.com>
2021-06-14 16:30:38 -0700

    clean: formatting and import for ApiDateTime

--- src/Api/ApiDateTime.php
+++ src/Api/ApiDateTime.php
@@ -7,18 +7,19 @@ namespace App\Api;
 use App\Entity\User;
 use DateTime;
 use DateTimeZone;
+use Exception;
 
 class ApiDateTime
 {
     public static function formatUserDate(DateTime $dateTime, User $user, string $format): string
     {
         $dateTz = $dateTime->setTimezone(new DateTimeZone($user->getTimezone()));
-        if($format === 'date') {
+        if ($format === 'date') {
             return $dateTz->format($user->getDateFormat());
-        } elseif($format === 'today') {
+        } elseif ($format === 'today') {
             return $dateTz->format($user->getTodayDateFormat());
         } else {
-            throw new \Exception("Unknown date format. Only 'date' and 'today' are supported. '$format' was given");
+            throw new Exception("Unknown date format. Only 'date' and 'today' are supported. '$format' was given");
         }
     }
 }
