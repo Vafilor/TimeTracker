065ab2c8224fc9ca26462a68153aba5ca308333f
Andrey Melnikov <vafilor@gmail.com>
2021-06-13 20:22:36 -0700

    feat: added AssignableToUserTrait to have a consistent, convenient way to indicate an Entity can be assigned to a user. This used to be 'createdBy' but someone else could create, say, a time entry for someone, but not be 'assigned' to it.

---
+++ src/Traits/AssignableToUserTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use App\Entity\User;
+
+/**
+ * Trait AssignableTrait
+ * @package App\Traits
+ *
+ * Provides convenience methods for any Entity that is assignedTo a user.
+ * Required: the entity has a property called $assignedTo.
+ *
+ * @property User $assignedTo
+ */
+trait AssignableToUserTrait
+{
+    public function getAssignedTo(): User
+    {
+        return $this->assignedTo;
+    }
+
+    public function assignTo(User $assignee): static
+    {
+        $this->assignedTo = $assignee;
+
+        return $this;
+    }
+
+    public function isAssignedTo(User $user): bool
+    {
+        return $this->assignedTo->equalIds($user);
+    }
+}
