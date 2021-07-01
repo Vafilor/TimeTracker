db9e12e15e2713303c9d3c627bf9590fc4b1da38
Andrey Melnikov <vafilor@gmail.com>
2021-06-30 18:49:01 -0700

    feat: created utilities for autocomplete and updated styling so it is clearer when it is in focus

--- assets/ts/components/time_entry_task_assigner.ts
+++ assets/ts/components/time_entry_task_assigner.ts
@@ -5,6 +5,7 @@ import { TimeEntryApi, TimeEntryApiErrorCode } from "../core/api/time_entry_api"
 import { ApiErrorResponse } from "../core/api/api";
 import AutocompleteTask from "./autocomplete_task";
 import { AutocompleteEnterPressedEvent } from "./autocomplete";
+import IdGenerator from "./id_generator";
 
 export class TimeEntryTaskAssigner {
     private readonly timeEntryId: string;
@@ -14,27 +15,31 @@ export class TimeEntryTaskAssigner {
     private task?: ApiTask;
 
     static template(taskId: string = '', taskName: string = '', taskUrl: string = ''): string {
+        const id = IdGenerator.next();
+
         return `
         <div 
             class="autocomplete js-autocomplete js-autocomplete-task js-time-entry-task-assigner"
             data-task-id="${taskId}"
             data-task-name="${taskName}"
             data-task-url="${taskUrl}">
-            <div class="d-flex">
-                <div class="search border-right-0 rounded-right-0">
+            <div class="autocomplete-search-group">
+                <div class="search">
+                    <label class="sr-only" for="autocomplete-task-${id}">name</label>
                     <input
+                            id="autocomplete-task-${id}"
                             type="text"
-                            class="js-input"
-                            placeholder="task name..."
+                            class="js-input form-control unset-height"
+                            placeholder="task name"
                             name="task"
                             autocomplete="off">
                     <button class="clear js-clear btn btn-sm"><i class="fas fa-times"></i></button>
                 </div>
-                <button type="button" class="btn js-delete btn-outline-danger rounded-left-0">
+                <button type="button" class="btn js-delete btn-outline-danger autocomplete-search-group-append">
                     <i class="fas fa-trash"></i>
                 </button>   
+                <div class="search-results d-none js-search-results"></div>
             </div>
-            <div class="search-results js-search-results d-none"></div>
         </div>`
     }
 
