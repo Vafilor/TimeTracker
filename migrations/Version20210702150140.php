2a93a3ab98680b189612d7b7bb6d793e1d08c65a
Andrey Melnikov <vafilor@gmail.com>
2021-07-02 10:54:24 -0700

    feat: added sqlite migration for statistics

--- migrations/Version20210702150140.php
+++ migrations/Version20210702150140.php
@@ -80,13 +80,64 @@ final class Version20210702150140 extends AbstractMigration
     protected function upSqlite(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        // do nothing
+        $this->addSql('CREATE TABLE statistic (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , assigned_to_id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , icon VARCHAR(255) DEFAULT NULL, name VARCHAR(255) NOT NULL, canonical_name VARCHAR(255) NOT NULL, description CLOB NOT NULL, color VARCHAR(7) NOT NULL, unit VARCHAR(255) NOT NULL, time_type VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_649B469CF4BD7827 ON statistic (assigned_to_id)');
+        $this->addSql('CREATE TABLE statistic_value (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , statistic_id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , time_entry_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , timestamp_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , value DOUBLE PRECISION NOT NULL, started_at DATETIME NOT NULL, ended_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_FC7CA2053B6268F ON statistic_value (statistic_id)');
+        $this->addSql('CREATE INDEX IDX_FC7CA201EB30A8E ON statistic_value (time_entry_id)');
+        $this->addSql('CREATE INDEX IDX_FC7CA202F202E84 ON statistic_value (timestamp_id)');
+
+        $this->addSql('DROP INDEX IDX_D8A326478DB60186');
+        $this->addSql('DROP INDEX IDX_D8A326471EB30A8E');
+        $this->addSql('DROP INDEX IDX_D8A326472F202E84');
+        $this->addSql('DROP INDEX IDX_D8A32647BAD26311');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__tag_link AS SELECT id, time_entry_id, timestamp_id, tag_id, task_id FROM tag_link');
+        $this->addSql('DROP TABLE tag_link');
+        $this->addSql('CREATE TABLE tag_link (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time_entry_id CHAR(36) DEFAULT NULL COLLATE BINARY --(DC2Type:uuid)
+        , timestamp_id CHAR(36) DEFAULT NULL COLLATE BINARY --(DC2Type:uuid)
+        , tag_id CHAR(36) NOT NULL COLLATE BINARY --(DC2Type:uuid)
+        , task_id CHAR(36) DEFAULT NULL COLLATE BINARY --(DC2Type:uuid)
+        , statistic_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , CONSTRAINT FK_D8A326471EB30A8E FOREIGN KEY (time_entry_id) REFERENCES time_entry (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_D8A326472F202E84 FOREIGN KEY (timestamp_id) REFERENCES timestamp (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_D8A326478DB60186 FOREIGN KEY (task_id) REFERENCES task (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_D8A32647BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_D8A3264753B6268F FOREIGN KEY (statistic_id) REFERENCES statistic (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO tag_link (id, time_entry_id, timestamp_id, tag_id, task_id) SELECT id, time_entry_id, timestamp_id, tag_id, task_id FROM __temp__tag_link');
+        $this->addSql('DROP TABLE __temp__tag_link');
+        $this->addSql('CREATE INDEX IDX_D8A326478DB60186 ON tag_link (task_id)');
+        $this->addSql('CREATE INDEX IDX_D8A326471EB30A8E ON tag_link (time_entry_id)');
+        $this->addSql('CREATE INDEX IDX_D8A326472F202E84 ON tag_link (timestamp_id)');
+        $this->addSql('CREATE INDEX IDX_D8A32647BAD26311 ON tag_link (tag_id)');
+        $this->addSql('CREATE INDEX IDX_D8A3264753B6268F ON tag_link (statistic_id)');
     }
 
     protected function downSqlite(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        // do nothing
+        $this->addSql('DROP TABLE statistic');
+        $this->addSql('DROP TABLE statistic_value');
+
+        $this->addSql('DROP INDEX IDX_D8A326471EB30A8E');
+        $this->addSql('DROP INDEX IDX_D8A326472F202E84');
+        $this->addSql('DROP INDEX IDX_D8A326478DB60186');
+        $this->addSql('DROP INDEX IDX_D8A32647BAD26311');
+        $this->addSql('DROP INDEX IDX_D8A3264753B6268F');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__tag_link AS SELECT id, time_entry_id, timestamp_id, task_id, tag_id FROM tag_link');
+        $this->addSql('DROP TABLE tag_link');
+        $this->addSql('CREATE TABLE tag_link (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time_entry_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , timestamp_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , task_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , tag_id CHAR(36) NOT NULL --(DC2Type:uuid)
+        )');
+        $this->addSql('INSERT INTO tag_link (id, time_entry_id, timestamp_id, task_id, tag_id) SELECT id, time_entry_id, timestamp_id, task_id, tag_id FROM __temp__tag_link');
+        $this->addSql('DROP TABLE __temp__tag_link');
+        $this->addSql('CREATE INDEX IDX_D8A326471EB30A8E ON tag_link (time_entry_id)');
+        $this->addSql('CREATE INDEX IDX_D8A326472F202E84 ON tag_link (timestamp_id)');
+        $this->addSql('CREATE INDEX IDX_D8A326478DB60186 ON tag_link (task_id)');
+        $this->addSql('CREATE INDEX IDX_D8A32647BAD26311 ON tag_link (tag_id)');
     }
 
     public function up(Schema $schema) : void
