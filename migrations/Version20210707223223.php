cf704ed9e389613f2560fcc82d3efaa55a49e33e
Andrey Melnikov <vafilor@gmail.com>
2021-07-08 17:47:45 -0700

    feat: updated migration to include blanks for other databases

--- migrations/Version20210707223223.php
+++ migrations/Version20210707223223.php
@@ -14,10 +14,10 @@ final class Version20210707223223 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Creates notes resources.';
+        return 'Creates notes resources';
     }
 
-    public function up(Schema $schema): void
+    public function upPostgresql(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE note (id UUID NOT NULL, assigned_to_id UUID NOT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
@@ -31,12 +31,73 @@ final class Version20210707223223 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_D8A3264726ED0855 ON tag_link (note_id)');
     }
 
-    public function down(Schema $schema): void
+    public function downPostgresql(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE tag_link DROP CONSTRAINT FK_D8A3264726ED0855');
         $this->addSql('DROP TABLE note');
         $this->addSql('DROP INDEX IDX_D8A3264726ED0855');
         $this->addSql('ALTER TABLE tag_link DROP note_id');
+    }
+
+    protected function upMysql(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    protected function downMysql(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+
+    protected function upSqlite(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    protected function downSqlite(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $platformName = $this->platform->getName();
+        switch ($platformName) {
+            case 'sqlite':
+                $this->upSqlite($schema);
+                break;
+            case 'postgresql':
+                $this->upPostgresql($schema);
+                break;
+            case 'mysql':
+                $this->upMysql($schema);
+                break;
+            default:
+                throw new \Exception("Unsupported database '{$platformName}'");
+        }
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $platformName = $this->platform->getName();
+        switch ($platformName) {
+            case 'sqlite':
+                $this->downSqlite($schema);
+                break;
+            case 'postgresql':
+                $this->downPostgresql($schema);
+                break;
+            case 'mysql':
+                $this->downMysql($schema);
+                break;
+            default:
+                throw new \Exception("Unsupported database '{$platformName}'");
+        }
     }
 }
