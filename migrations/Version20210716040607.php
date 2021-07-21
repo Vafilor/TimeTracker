01ca85b3cbd6f17963e4be09983b8d1518bfeade
Andrey Melnikov <vafilor@gmail.com>
2021-07-21 15:35:16 -0700

    feat: added migration for sqlite for task updates

--- migrations/Version20210716040607.php
+++ migrations/Version20210716040607.php
@@ -14,10 +14,10 @@ final class Version20210716040607 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Updates tasks to have due dates, soft deletes, and subtasks';
     }
 
-    public function up(Schema $schema): void
+    public function upPostgresql(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE task ADD parent_id UUID DEFAULT NULL');
@@ -28,7 +28,7 @@ final class Version20210716040607 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_527EDB25727ACA70 ON task (parent_id)');
     }
 
-    public function down(Schema $schema): void
+    public function downPostgresql(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB25727ACA70');
@@ -36,5 +36,74 @@ final class Version20210716040607 extends AbstractMigration
         $this->addSql('ALTER TABLE task DROP parent_id');
         $this->addSql('ALTER TABLE task DROP due_at');
         $this->addSql('ALTER TABLE task DROP deleted_at');
+    }
+
+    public function upSqlite(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_527EDB25F4BD7827');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__task AS SELECT id, assigned_to_id, name, description, canonical_name, priority, created_at, completed_at, updated_at FROM task');
+        $this->addSql('DROP TABLE task');
+        $this->addSql('CREATE TABLE task (id CHAR(36) NOT NULL COLLATE BINARY --(DC2Type:uuid)
+        , assigned_to_id CHAR(36) NOT NULL COLLATE BINARY --(DC2Type:uuid)
+        , parent_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , name VARCHAR(255) NOT NULL COLLATE BINARY, description CLOB NOT NULL COLLATE BINARY, canonical_name VARCHAR(255) NOT NULL COLLATE BINARY, priority INTEGER NOT NULL, created_at DATETIME NOT NULL, completed_at DATETIME DEFAULT NULL, updated_at DATETIME NOT NULL, due_at DATETIME DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id), CONSTRAINT FK_527EDB25F4BD7827 FOREIGN KEY (assigned_to_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_527EDB25727ACA70 FOREIGN KEY (parent_id) REFERENCES task (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO task (id, assigned_to_id, name, description, canonical_name, priority, created_at, completed_at, updated_at) SELECT id, assigned_to_id, name, description, canonical_name, priority, created_at, completed_at, updated_at FROM __temp__task');
+        $this->addSql('DROP TABLE __temp__task');
+        $this->addSql('CREATE INDEX IDX_527EDB25F4BD7827 ON task (assigned_to_id)');
+        $this->addSql('CREATE INDEX IDX_527EDB25727ACA70 ON task (parent_id)');
+    }
+
+    public function downSqlite(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_527EDB25F4BD7827');
+        $this->addSql('DROP INDEX IDX_527EDB25727ACA70');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__task AS SELECT id, assigned_to_id, completed_at, name, canonical_name, description, priority, created_at, updated_at FROM task');
+        $this->addSql('DROP TABLE task');
+        $this->addSql('CREATE TABLE task (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , assigned_to_id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , completed_at DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, canonical_name VARCHAR(255) NOT NULL, description CLOB NOT NULL, priority INTEGER NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO task (id, assigned_to_id, completed_at, name, canonical_name, description, priority, created_at, updated_at) SELECT id, assigned_to_id, completed_at, name, canonical_name, description, priority, created_at, updated_at FROM __temp__task');
+        $this->addSql('DROP TABLE __temp__task');
+        $this->addSql('CREATE INDEX IDX_527EDB25F4BD7827 ON task (assigned_to_id)');
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $platformName = $this->platform->getName();
+        switch ($platformName) {
+            case 'sqlite':
+                $this->upSqlite($schema);
+                break;
+            case 'postgresql':
+                $this->upPostgresql($schema);
+                break;
+            case 'mysql':
+                $this->upMysql($schema);
+                break;
+            default:
+                throw new Exception("Unsupported database '{$platformName}'");
+        }
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $platformName = $this->platform->getName();
+        switch ($platformName) {
+            case 'sqlite':
+                $this->downSqlite($schema);
+                break;
+            case 'postgresql':
+                $this->downPostgresql($schema);
+                break;
+            case 'mysql':
+                $this->downMysql($schema);
+                break;
+            default:
+                throw new Exception("Unsupported database '{$platformName}'");
+        }
     }
 }
