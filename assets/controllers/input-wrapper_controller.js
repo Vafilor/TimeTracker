2f704c72d77452599408eb3b0de5ff7acd4e0a88
Andrey Melnikov <vafilor@gmail.com>
2021-08-12 17:46:50 -0700

    clean: remove unused controller

--- assets/controllers/input-wrapper_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from 'stimulus';
-
-export default class extends Controller {
-    static targets = ['target'];
-
-    updateTarget(event) {
-        console.log('update target', event);
-    }
-}
