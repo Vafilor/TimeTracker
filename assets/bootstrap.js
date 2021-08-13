798059ea93d89e869726fbe77ec73bfc60784fe6
Andrey Melnikov <vafilor@gmail.com>
2021-08-12 18:32:40 -0700

    fix: remove unused import

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,4 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-import { Autocomplete } from "stimulus-autocomplete";
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
