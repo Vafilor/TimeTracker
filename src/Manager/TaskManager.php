dbec3c5d3d5a94a80aad7cd33640c0124fd1ef9f
Andrey Melnikov <vafilor@gmail.com>
2021-09-09 19:38:12 -0700

    fix: added missing TaskManager

---
+++ src/Manager/TaskManager.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Manager;
+
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
+use Ramsey\Uuid\Uuid;
+use SplStack;
+
+class TaskManager
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function applyTemplate(Task $task, Task $taskTemplate)
+    {
+        $stack = new SplStack();
+        foreach ($taskTemplate->getSubtasks() as $subtask) {
+            $copy = clone $subtask;
+            $copy->setId(Uuid::uuid4());
+            $copy->setParent($task);
+            $copy->setTemplate(false);
+            $stack->push($copy);
+            $this->entityManager->persist($copy);
+        }
+
+        while (!$stack->isEmpty()) {
+            /** @var Task $newTask */
+            $newTask = $stack->pop();
+
+            foreach ($newTask->getSubtasks() as $subtask) {
+                $copy = clone $subtask;
+                $copy->setId(Uuid::uuid4());
+                $copy->setParent($newTask);
+                $copy->setTemplate(false);
+                $stack->push($copy);
+                $this->entityManager->persist($copy);
+            }
+        }
+    }
+}
