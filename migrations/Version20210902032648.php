f90b38345adb0060338dfe390d5dff8972684dcb
Andrey Melnikov <vafilor@gmail.com>
2021-09-09 19:59:18 -0700

    feat: sqlite migration for task template

--- migrations/Version20210902032648.php
+++ migrations/Version20210902032648.php
@@ -49,12 +49,35 @@ final class Version20210902032648 extends AbstractMigration
     public function upSqlite(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_527EDB25727ACA70');
+        $this->addSql('DROP INDEX IDX_527EDB25F4BD7827');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__task AS SELECT id, assigned_to_id, parent_id, name, description, canonical_name, priority, created_at, completed_at, updated_at, due_at, deleted_at FROM task');
+        $this->addSql('DROP TABLE task');
+        $this->addSql('CREATE TABLE task (id CHAR(36) NOT NULL COLLATE BINARY --(DC2Type:uuid)
+        , assigned_to_id CHAR(36) NOT NULL COLLATE BINARY --(DC2Type:uuid)
+        , parent_id CHAR(36) DEFAULT NULL COLLATE BINARY --(DC2Type:uuid)
+        , name VARCHAR(255) NOT NULL COLLATE BINARY, description CLOB NOT NULL COLLATE BINARY, canonical_name VARCHAR(255) NOT NULL COLLATE BINARY, priority INTEGER NOT NULL, created_at DATETIME NOT NULL, completed_at DATETIME DEFAULT NULL, updated_at DATETIME NOT NULL, due_at DATETIME DEFAULT NULL, deleted_at DATETIME DEFAULT NULL, template BOOLEAN NOT NULL, PRIMARY KEY(id), CONSTRAINT FK_527EDB25F4BD7827 FOREIGN KEY (assigned_to_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_527EDB25727ACA70 FOREIGN KEY (parent_id) REFERENCES task (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO task (id, assigned_to_id, parent_id, name, description, canonical_name, priority, created_at, completed_at, updated_at, due_at, deleted_at, template) SELECT id, assigned_to_id, parent_id, name, description, canonical_name, priority, created_at, completed_at, updated_at, due_at, deleted_at, false FROM __temp__task');
+        $this->addSql('DROP TABLE __temp__task');
+        $this->addSql('CREATE INDEX IDX_527EDB25727ACA70 ON task (parent_id)');
+        $this->addSql('CREATE INDEX IDX_527EDB25F4BD7827 ON task (assigned_to_id)');
     }
 
     public function downSqlite(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('DROP INDEX IDX_527EDB25F4BD7827');
+        $this->addSql('DROP INDEX IDX_527EDB25727ACA70');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__task AS SELECT id, assigned_to_id, parent_id, completed_at, name, canonical_name, description, priority, due_at, created_at, updated_at, deleted_at FROM task');
+        $this->addSql('DROP TABLE task');
+        $this->addSql('CREATE TABLE task (id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , assigned_to_id CHAR(36) NOT NULL --(DC2Type:uuid)
+        , parent_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
+        , completed_at DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, canonical_name VARCHAR(255) NOT NULL, description CLOB NOT NULL, priority INTEGER NOT NULL, due_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO task (id, assigned_to_id, parent_id, completed_at, name, canonical_name, description, priority, due_at, created_at, updated_at, deleted_at) SELECT id, assigned_to_id, parent_id, completed_at, name, canonical_name, description, priority, due_at, created_at, updated_at, deleted_at FROM __temp__task');
+        $this->addSql('DROP TABLE __temp__task');
+        $this->addSql('CREATE INDEX IDX_527EDB25F4BD7827 ON task (assigned_to_id)');
+        $this->addSql('CREATE INDEX IDX_527EDB25727ACA70 ON task (parent_id)');
     }
 
     public function up(Schema $schema) : void
