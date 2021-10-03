34bf52d8e27e1129aa1b756f8ba4d3617300da47
Andrey Melnikov <vafilor@gmail.com>
2021-10-02 22:02:34 -0700

    fix: note detail tags not working

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -158,7 +158,6 @@ class NoteController extends BaseController
             [
                 'note' => $note,
                 'form' => $form->createView(),
-                'tags' => ApiTag::fromEntities($note->getTags()),
             ]
         );
     }
