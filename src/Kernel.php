37c246937100f4f1bc16aa9dd4a0d96f0e94a198
Andrey Melnikov <vafilor@gmail.com>
2022-11-07 19:03:47 -0800

    Remove configureRoutes as it is in the micro kernel trait now

--- src/Kernel.php
+++ src/Kernel.php
@@ -23,16 +23,4 @@ class Kernel extends BaseKernel
             (require $path)($container->withPath($path), $this);
         }
     }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
-
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
-            (require $path)($routes->withPath($path), $this);
-        }
-    }
 }
